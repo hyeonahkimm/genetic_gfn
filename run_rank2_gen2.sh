3f6c800beb573da6c7e46ef6f9960a795fe3a2f9
Hyeonah Kim <gusk6415@gmail.com>
2023-12-20 13:14:16 +0900

    script

---
+++ run_rank2_gen2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+oracle_array=('jnk3' 'drd2' 'qed' 'gsk3b' 'celecoxib_rediscovery' 'troglitazone_rediscovery' \
+        'thiothixene_rediscovery' 'albuterol_similarity' 'mestranol_similarity' \
+        'isomers_c7h8n2o2' 'isomers_c9h10n2o2pf2cl' 'median1' 'median2' 'osimertinib_mpo' \
+        'fexofenadine_mpo' 'ranolazine_mpo' 'perindopril_mpo' 'amlodipine_mpo' \
+        'sitagliptin_mpo' 'zaleplon_mpo' 'valsartan_smarts' 'deco_hop' 'scaffold_hop')
+
+for seed in 0 1 2
+do
+for oralce in "${oracle_array[@]}"
+do
+# echo $oralce
+CUDA_VISIBLE_DEVICES=1 python run.py reinvent_ga --task simple --config_default 'hparams_rank2_gen3.yaml' --wandb online --run_name rank2xgen3_prior_rank2 --oracle $oralce --seed $seed
+done
+done
