d43d3970311fa39ccff7d48e23cf593b45ec67d8
Hyeonah Kim <gusk6415@gmail.com>
2024-05-08 20:03:24 +0900

    genetic_gfn-al

--- tune_dropout.sh
+++ tune_dropout.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-model="genetic_gfn_al"
+model="reinvent_ga"
 
 nohup python -u run.py ${model} \
     --n_jobs 1 --task tune --n_runs 50 --wandb online --config_default hparams_al.yaml --config_tune hparams_tune.yaml \
-    --oracles zaleplon_mpo perindopril_mpo > tune_lr_${model}.out 2>&1 &
+    --oracles jnk3 isomers_c9h10n2o2pf2cl zaleplon_mpo perindopril_mpo > tune_lr_${model}.out 2>&1 &
 
 # CUDA_VISIBLE_DEVICES=1 bash tune.sh
